8e17a08e7948179c8a13ef286ddab9ed3c7ba09e
Yanis Plumit <plumit@mqd.me>
2013-08-21 14:51:51 +0400

    first commit

---
+++ AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Twitter iOS5
+//
+//  Created by James Shaw on 20/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
